a7474d1b158f013aaf06b16653fc5db8e2aeb0db
Pennino Cristian <penninocristianfrancesco@gmail.com>
2025-08-28 11:11:56 +0200

    Implement LeftSidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@ import { auth } from "@/auth";
 const Home = async () => {
   const session = await auth();
 
+  console.log(session);
   return (
     <>
       <h1 className="text-3xl font-bold text-blue-500">Home</h1>
